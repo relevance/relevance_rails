7d6eb7cc6e57d947bf264d696a1300feb445b06e
Alex Redington and Gabriel Horner <pair@thinkrelevance.com>
2012-03-30 09:35:56 -0400

    Move generators into lib/generators, make relevance_rails a dependency in the Gemfile

--- lib/generators/relevance_file/relevance_file_generator.rb
+++ lib/generators/relevance_file/relevance_file_generator.rb
@@ -38,10 +38,10 @@ class RelevanceFileGenerator < Rails::Generators::NamedBase
     copy_file 'Capfile', 'Capfile'
   end
 
-  def copy_generators
-    copy_file 'deployment_generator.rb', 'lib/generators/deployment/deployment_generator.rb'
-    copy_file 'fixtures_generator.rb', 'lib/generators/fixtures/fixtures_generator.rb'
-  end
+  # def copy_generators
+  #   copy_file 'deployment_generator.rb', 'lib/generators/deployment/deployment_generator.rb'
+  #   copy_file 'fixtures_generator.rb', 'lib/generators/fixtures/fixtures_generator.rb'
+  # end
 
   def create_deploy
     template 'deploy.rb.erb', 'config/deploy.rb'
