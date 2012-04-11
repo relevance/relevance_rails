136a7a77ff7d70d649b05c4ae792b6aff9a019ba
Relevance Pair <pair@thinkrelevance.com>
2012-04-11 16:53:57 -0400

    SPIKE: Monkey-patched fog to display interim output for long SSH commands (like Chef converges!)

--- lib/relevance_rails/provision.rb
+++ lib/relevance_rails/provision.rb
@@ -1,6 +1,57 @@
 require 'fog'
+require 'fog/core/ssh'
 require 'thor'
 require 'timeout'
+
+#Duck Punch us some Fog
+class Fog::SSH::Real
+  def run(commands)
+    commands = [*commands]
+    results  = []
+    begin
+      Net::SSH.start(@address, @username, @options) do |ssh|
+        commands.each do |command|
+          result = Fog::SSH::Result.new(command)
+          ssh.open_channel do |ssh_channel|
+            ssh_channel.request_pty
+            ssh_channel.exec(command) do |channel, success|
+              unless success
+                raise "Could not execute command: #{command.inspect}"
+              end
+
+              channel.on_data do |ch, data|
+                result.stdout << data
+                puts data
+              end
+
+              channel.on_extended_data do |ch, type, data|
+                next unless type == 1
+                result.stderr << data
+                puts data
+              end
+
+              channel.on_request('exit-status') do |ch, data|
+                result.status = data.read_long
+              end
+
+              channel.on_request('exit-signal') do |ch, data|
+                result.status = 255
+              end
+            end
+          end
+          ssh.loop
+          results << result
+        end
+      end
+    rescue Net::SSH::HostKeyMismatch => exception
+      exception.remember_host!
+      sleep 0.2
+      retry
+    end
+    results
+  end
+end
+
 
 module RelevanceRails
   module Provision
