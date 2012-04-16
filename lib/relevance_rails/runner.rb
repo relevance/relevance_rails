a40fea27908d8f291ae5e7975ce24ac0df134835
Larry Karnowski <larry@hickorywind.org>
2012-04-16 15:19:11 -0400

    GH #51: support RVM versions of 1.12.0 and later

--- lib/relevance_rails/runner.rb
+++ lib/relevance_rails/runner.rb
@@ -49,7 +49,12 @@ STR
         abort "Rvm version 1.10.2 or greater is required. Run 'rvm get stable'"
       end
 
-      $LOAD_PATH.unshift "#{ENV['rvm_path']}/lib"
+      if rvm_version < Gem::Version.new('1.12.0')
+        # RVM's ruby drivers were factored out into a gem
+        # in 1.12.0, so you don't use this trick anymore.
+        $LOAD_PATH.unshift "#{ENV['rvm_path']}/lib"
+      end
+
       require 'rvm'
 
       env = RVM::Environment.current
