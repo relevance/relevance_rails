cefeea345d770c2da7a36ae77ff045b980b5e7e1
Alex Redington <alex.redington@thinkrelevance.com>
2012-04-19 14:58:42 -0400

    - Add AptInstaller exception when we try to run apt and it fails. - Retry logic abstracted away from where it's used. - All apt commands get their own function to associate the retry - Add test on wait_for_ssh - Add test on apt_installs

--- spec/lib/provision_spec.rb
+++ spec/lib/provision_spec.rb
@@ -13,4 +13,35 @@ describe RelevanceRails::Provision do
       end.should =~ /STDERR: FAIL WHALE/
     end
   end
+
+  describe '.wait_for_ssh' do
+    it 'retries up to 5 times, then fails' do
+      server = mock("server")
+      server.should_receive(:wait_for).ordered.and_return(true)
+      server.should_receive(:ssh).ordered.and_raise(Errno::ECONNREFUSED)
+      server.should_receive(:ssh).ordered.and_return([mock(:status => 0, :stdout => '', :stderr => '', :command => 'echo')])
+      STDOUT.should_receive(:puts).with("Waiting for ssh connectivity...")
+      STDOUT.should_receive(:puts).with("Connecting to Amazon refused. Retrying...")
+      STDOUT.should_receive(:puts).with("Server up and listening for SSH!")
+      RelevanceRails::Provision.should_receive(:sleep).twice.with(10).and_return(10)
+      expect do
+        RelevanceRails::Provision.wait_for_ssh(server)
+      end.to_not raise_error
+    end
+  end
+
+  describe '.apt_installs' do
+    it 'retries twice, then fails' do
+      server = mock("server")
+      server.should_receive(:ssh).ordered.with('sudo apt-get update').and_return([mock(:status => 0, :stdout => 'waha! no ruby for you...', :stderr => '', :command => 'sudo apt-get update')])
+      server.should_receive(:ssh).ordered.with('sudo apt-get -y install ruby').and_return([mock(:status => 1, :stdout => '', :stderr => 'no ruby candidate, I hate you', :command => 'sudo apt-get -y install ruby')])
+      server.should_receive(:ssh).ordered.with('sudo apt-get update').and_return([mock(:status => 0, :stdout => 'i know how to do ruby now', :stderr => '', :command => 'sudo apt-get update')])
+      server.should_receive(:ssh).ordered.with('sudo apt-get -y install ruby').and_return([mock(:status => 0, :stdout => 'ruby installed!', :stderr => '', :command => 'sudo apt-get -y install ruby')])
+      server.should_receive(:ssh).ordered.with('sudo apt-get -y install rubygems1.8').and_return([mock(:status => 0, :stdout => 'rubygems installed!', :stderr => '', :command => 'sudo apt-get -y install rubygems1.8')])
+      STDOUT.should_receive(:puts).any_number_of_times
+      expect do
+        RelevanceRails::Provision.apt_installs(server)
+      end.to_not raise_error
+    end
+  end
 end
